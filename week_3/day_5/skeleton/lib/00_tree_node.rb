110b1ef1aa49fc24bbf9bd7678d192327efea553
Abraham Fong <abisfong@ucsc.edu>
2021-09-17 16:24:08 -0700

    Create tree searching methods DFS and BFS

--- week_3/day_5/skeleton/lib/00_tree_node.rb
+++ week_3/day_5/skeleton/lib/00_tree_node.rb
@@ -9,15 +9,37 @@ class PolyTreeNode
 
   def parent=(val)
     if parent != nil
-      parent.children.delete(self)
+      @parent.children.delete(self)
     end
-    parent = val
+    @parent = val
     val.children << self if val != nil
-    
   end
 
-  def inspect
-    
+  def add_child(child)
+    child.parent = self
   end
 
+  def remove_child(child)
+    raise "Not a child" if !@children.include?(child)
+    child.parent = nil
+  end
+
+  def dfs(target)
+    return self if self.value == target
+    self.children.each do |child|
+      res = child.dfs(target)
+      return res if res
+    end
+    nil
+  end
+
+  def bfs(target)
+    queue = [self]
+    while !queue.empty?
+      node = queue.shift
+      return node if node.value == target
+      queue.concat(node.children)
+    end
+    nil
+  end
 end
