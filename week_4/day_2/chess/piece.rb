948c0983850a703deb6cb63d886fd4ef3aa552cf
Abraham Fong <abisfong@ucsc.edu>
2021-09-21 16:23:54 -0700

    Begin defining Piece instance methods

--- week_4/day_2/chess/piece.rb
+++ week_4/day_2/chess/piece.rb
@@ -1,3 +1,14 @@
 class Piece
+  def initialize(color, board, pos=[])
+    @color = color
+    @board = board
+    @pos = pos
+  end
 
+  def to_s
+    "#{@color}, #{@pos}"
+  end
+
+  def empty?
+  end
 end
