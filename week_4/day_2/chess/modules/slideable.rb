af4d052a62ca3c7656a3b5b17aab6383d2bfcfef
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-09-21 17:31:51 -0700

    Complete grow_unblocked_moves_in_dir method and start NullPiece class

--- week_4/day_2/chess/modules/slideable.rb
+++ week_4/day_2/chess/modules/slideable.rb
@@ -7,6 +7,7 @@ module Slideable
       dy, dx = move_dir
       possible_moves.concat(grow_unblocked_moves_in_dir(dx, dy))
     end
+    possible_moves
     #use moves_dirs here
     #uses grow unblocked moves
   end
@@ -30,5 +31,14 @@ module Slideable
   def grow_unblocked_moves_in_dir(dx,dy)
     # Add dx and dy to @pos and return array of all unblock positions
     # Add logic for when to stop
+    moves = []
+
+    next_pos = [@pos.first + dy, @pos.last + dx]
+    while @board[next_pos].is_a?(NullPiece)
+      moves << next_pos
+      next_pos = [next_pos.first + dy, next_pos.last + dx]
+    end
+
+    moves
   end
 end
