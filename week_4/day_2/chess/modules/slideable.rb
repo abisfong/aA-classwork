550e5af98f08854646d0fea3a64a1d1855f6cfd7
Abraham Fong <abisfong@ucsc.edu>
2021-09-21 17:09:17 -0700

    Define moves method in Slideable module
    Created an array that represents moves in horizontal or vertical
    directions.
    
    Created an array that represents moves in diagonal directions.
    
    We are assuming that move_dirs is implemented in classes that
    include Slideable. move_dirs will return an array of the
    directions a sliding piece can move in.

--- week_4/day_2/chess/modules/slideable.rb
+++ week_4/day_2/chess/modules/slideable.rb
@@ -2,26 +2,33 @@ module Slideable
   #@color, @board, @pos (from piece class)
 
   def moves #returns a array of places that it can move to
-    self.move_dirs
+    possible_moves = []
+    move_dirs.each do |move_dir|
+      dy, dx = move_dir
+      possible_moves.concat(grow_unblocked_moves_in_dir(dx, dy))
+    end
     #use moves_dirs here
     #uses grow unblocked moves
   end
 
   def horizontal_dirs
-    [ [0,1], [1,0], [0,-1], [-1,0] ]
+    HORIZONTAL_DIRS
   end
 
   def diagonal_dirs
-    [ [1,1], [1,-1], [-1,1], [-1,-1] ]
+    DIAGONAL_DIRS
   end
-
+  
   private
-
+  HORIZONTAL_DIRS = [ [0,1], [1,0], [0,-1], [-1,0] ]
+  DIAGONAL_DIRS = [ [1,1], [1,-1], [-1,1], [-1,-1] ]
+  
   def move_dirs #indicate which direction it can move
     
   end
 
   def grow_unblocked_moves_in_dir(dx,dy)
-
+    # Add dx and dy to @pos and return array of all unblock positions
+    # Add logic for when to stop
   end
 end
