cb4223c1cf90dba59ade3b9588b4f98f4f9514cc
jacky <jc6825@stern.nyu.edu>
2021-09-30 19:51:15 -0400

    Complete associations

--- week_5/day_4/associations_exercise/db/schema.rb
+++ week_5/day_4/associations_exercise/db/schema.rb
@@ -15,20 +15,20 @@ ActiveRecord::Schema.define(version: 20170710184251) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.integer "prereq_id"
-    t.integer "instructor_id"
+    t.integer "instructor_id" 
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "enrollments", force: :cascade do |t|
-    t.integer "course_id"
-    t.integer "student_id"
+    t.integer "course_id" 
+    t.integer "student_id" 
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name" 
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
