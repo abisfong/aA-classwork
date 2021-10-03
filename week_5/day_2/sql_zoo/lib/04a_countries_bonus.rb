ab0a812a7e12383f5e7bf400d44168664f4fc7d5
Abraham Fong <abisfong@ucsc.edu>
2021-10-03 13:11:58 -0700

    Complete phase 3 and 4 bonus queries

--- week_5/day_2/sql_zoo/lib/04a_countries_bonus.rb
+++ week_5/day_2/sql_zoo/lib/04a_countries_bonus.rb
@@ -14,6 +14,19 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      countries.name
+    FROM
+      countries
+    WHERE
+      countries.continent != 'Europe' AND gdp > (
+        SELECT
+          MAX(gdp)
+        FROM
+          countries
+        WHERE
+          countries.continent = 'Europe'
+      )
   SQL
 end
 
@@ -21,6 +34,13 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT countries.continent, countries.name, countries.area
+    FROM countries
+    WHERE countries.area IN (
+      SELECT MAX(area)
+      FROM countries
+      GROUP BY countries.continent
+    )
   SQL
 end
 
@@ -28,5 +48,19 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT
+      outer_countries.name, outer_countries.continent
+    FROM
+      countries AS outer_countries
+    WHERE
+      outer_countries.population > ALL (
+        SELECT
+          (countries.population * 3) AS population
+        FROM
+          countries
+        WHERE
+          countries.continent = outer_countries.continent AND countries.name != outer_countries.name
+      )
+    
   SQL
 end
