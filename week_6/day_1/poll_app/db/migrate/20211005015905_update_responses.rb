ec486b0cd2e612dd0f355cda9652d7cb8d1e57f7
Abraham Fong <abisfong@ucsc.edu>
2021-10-04 19:03:10 -0700

    Change references in responses table for PollApp

--- week_6/day_1/poll_app/db/migrate/20211005015905_update_responses.rb
+++ week_6/day_1/poll_app/db/migrate/20211005015905_update_responses.rb
@@ -1,4 +1,6 @@
 class UpdateResponses < ActiveRecord::Migration[5.2]
   def change
+    remove_column :responses, :question_id
+    add_column :responses, :user_id, :integer, null: false
   end
 end
