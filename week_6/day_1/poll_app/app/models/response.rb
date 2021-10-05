1d5c8c191ba623219a54e58f4772e3e344397d75
Abraham Fong <abisfong@ucsc.edu>
2021-10-04 19:23:21 -0700

    Create model associations and validations

--- week_6/day_1/poll_app/app/models/response.rb
+++ week_6/day_1/poll_app/app/models/response.rb
@@ -1,2 +1,14 @@
 class Response < ApplicationRecord
+  validates :answer_choice_id, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :answer_choice,
+    class_name: :AnswerChoice,
+    primary_key: :id,
+    foreign_key: :answer_choice_id
+
+  belongs_to :respondent,
+    class_name: :User,
+    primary_key: :id,
+    foreign_key: :user_id
 end
--- week_6/day_1/poll_app/app/models/user.rb
+++ week_6/day_1/poll_app/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+
+  has_many :authored_polls,
+    class_name: :Polls,
+    primary_key: :id,
+    foreign_key: :author_id
+  
+  has_many :responses,
+    class_name: :Response,
+    primary_key: :id,
+    foreign_key: :user_id
 end
