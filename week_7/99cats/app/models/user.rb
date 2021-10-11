0daf84b6eb13837af3ac5eff2e1645f5e45c49ee
Abraham Fong <abisfong@ucsc.edu>
2021-10-11 14:50:42 -0700

    Complete User model

--- week_7/99cats/app/models/user.rb
+++ week_7/99cats/app/models/user.rb
@@ -10,13 +10,24 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  attr_reader :password
+
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  def self.find_under_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      return nil
+    end
+  end
 
   def reset_session_token!
     self.session_token = SecureRandom::urlsafe_base64
   end
-
 
   def password=(password)
     self.password_digest = BCrypt::Pasword.create(password)
@@ -27,5 +38,4 @@ class User < ApplicationRecord
     password_object = BCrypt::Password.new(self.password_digest)
     password_object.is_password?(password)
   end
-
 end
