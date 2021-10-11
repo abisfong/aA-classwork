477d4625a2a6301a340f53710da63dc7cec89752
Abraham Fong <abisfong@ucsc.edu>
2021-10-11 15:02:59 -0700

    Create UsersController

---
+++ week_7/99cats/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to cats_url
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
