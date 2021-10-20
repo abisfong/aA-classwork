86e46e2e26791554d9ba2268c1fc70527a47da43
Grayson Poon <grayson.a.poon@gmail.com>
2021-10-20 13:35:32 -0700

    Create grid function and assign color to pieces

--- week_8/day_3/reversi/src/board.js
+++ week_8/day_3/reversi/src/board.js
@@ -8,7 +8,19 @@ if (typeof window === 'undefined'){
  * Returns a 2D array (8 by 8) with two black pieces at [3, 4] and [4, 3]
  * and two white pieces at [3, 3] and [4, 4]
  */
-function _makeGrid () {
+function _makeGrid() {
+  let grid = new Array(8);
+
+  for (let i = 0; i < grid.length; i++)
+    grid[i] = new Array(8);
+
+  grid[3][3] = new Piece("white");
+  grid[4][4] = new Piece("white");
+
+  grid[3][4] = new Piece("black");
+  grid[4][3] = new Piece("black");
+
+  return grid;
 }
 
 /**
--- week_8/day_3/reversi/src/piece.js
+++ week_8/day_3/reversi/src/piece.js
@@ -1,7 +1,9 @@
 /**
  * Initializes the Piece with its color.
  */
-function Piece (color) {
+function Piece(color) {
+    this.color = color;
+
 }
 
 /**
@@ -14,6 +16,7 @@ Piece.prototype.oppColor = function () {
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    this.color = this.color === "white" ? "black" : "white";
 };
 
 /**
@@ -21,6 +24,7 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+    return this.color === "white" ? "W" : "B";
 };
 
 // DON'T TOUCH THIS CODE
