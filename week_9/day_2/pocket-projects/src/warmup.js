9810ee48273919580345684e4116343b0f5ce21c
Abraham Fong <abisfong@ucsc.edu>
2021-10-26 16:52:52 -0700

    Steal clock code

--- week_9/day_2/pocket-projects/src/warmup.js
+++ week_9/day_2/pocket-projects/src/warmup.js
@@ -2,7 +2,9 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-
+  let el = document.createElement('p')
+  el.append(string);
+  htmlElement.appendChild(el);
 };
 
 htmlGenerator('Party Time.', partyHeader);
