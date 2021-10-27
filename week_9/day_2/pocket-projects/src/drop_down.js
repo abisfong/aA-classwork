ce51bd7e0ac1216a12382a134abbab689e876147
kw-8 <kwu11235813@gmail.com>
2021-10-26 17:30:35 -0700

    halfway through dog links

--- week_9/day_2/pocket-projects/src/drop_down.js
+++ week_9/day_2/pocket-projects/src/drop_down.js
@@ -9,3 +9,22 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+function dogLinkCreator () {
+  let dogLinks = [];
+  for (let key in dogs) {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.setAttribute("href", dogs[key]);
+    a.innerHTML = key; //content of elt
+    li.classList.set("dog-link"); //all have class
+    li.appendChild(a);
+    dogLinks.push(li);
+  }
+}
+
+function attachDogLinks() {
+  let dogLinks = dogLinkCreator();
+  document.querySelector(".drop-down-dog-list").append(dogLinks);
+}
+
+export {attachDogLinks}
