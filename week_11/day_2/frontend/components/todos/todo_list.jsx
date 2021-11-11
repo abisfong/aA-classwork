06f095e765885459be8a24d89736dfd8cc7f707c
Abraham Fong <abisfong@ucsc.edu>
2021-11-10 16:33:06 -0800

    Create todo delete functionality

--- week_11/day_2/frontend/components/todos/todo_list.jsx
+++ week_11/day_2/frontend/components/todos/todo_list.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import TodoForm from "./todo_form";
 import TodoListItem from "./todo_list_item";
 
-const TodoList = ({receiveTodo, todos}) => (
+const TodoList = ({receiveTodo, removeTodo, todos}) => (
   <div>
     <ul>
-      {todos.map((todo, i) => <TodoListItem key={i} todo={todo} />)}
+      {todos.map((todo, i) => (
+        <TodoListItem removeTodo={removeTodo}  key={i} todo={todo} />
+      ))}
     </ul>
     <TodoForm receiveTodo={receiveTodo} todos={todos}/>
   </div>
--- week_11/day_2/frontend/components/todos/todo_list_item.jsx
+++ week_11/day_2/frontend/components/todos/todo_list_item.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const TodoListItem = ({todo}) => {
-  return <li>{todo.title}</li>
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const todo = this.props.todo;
+    this.props.removeTodo(todo);
+  }
+
+  render() {
+    const todo = this.props.todo;
+    return (
+      <li>
+        {todo.title}
+        <br />
+        {todo.body}
+        <br />
+        {todo.done === 'true' ? 'done' : 'not done'}
+        <button onClick={this.handleClick}>Delete</button>
+      </li>
+    );
+  }
 }
 
 export default TodoListItem;
