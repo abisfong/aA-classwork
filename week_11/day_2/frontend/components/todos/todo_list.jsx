c62922dfaaff247a4bf88db4854785d671e2c6aa
cjc473 <chris.cheasty3@gmail.com>
2021-11-10 16:03:59 -0800

    Complete project through phase III

--- week_11/day_2/frontend/components/todos/todo_list.jsx
+++ week_11/day_2/frontend/components/todos/todo_list.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import TodoForm from "./todo_form";
+import TodoListItem from "./todo_list_item";
 
-const TodoList = () => (
-  <h3>Todo list goes here</h3>
+const TodoList = ({receiveTodo, todos}) => (
+  <div>
+    <ul>
+      {todos.map((todo, i) => <TodoListItem key={i} todo={todo} />)}
+    </ul>
+    <TodoForm receiveTodo={receiveTodo} todos={todos}/>
+  </div>
 );
 
 export default TodoList;
