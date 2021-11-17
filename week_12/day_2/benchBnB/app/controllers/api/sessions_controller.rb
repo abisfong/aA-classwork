9857f11f2b078987077538ac75a9ded39019ec8c
Cal Brown <calsbrown4@gmail.com>
2021-11-16 16:19:22 -0800

    Push

--- week_12/day_2/benchBnB/app/controllers/api/sessions_controller.rb
+++ week_12/day_2/benchBnB/app/controllers/api/sessions_controller.rb
@@ -9,4 +9,20 @@ class Api::SessionsController < ApplicationController
     logout(current_user)
     render {}
   end
+
+  def create
+    # Find user by credentials
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    # Flash errors, if any.
+    # Render :new if invalid credentials (give the user another chance to login)
+    if @user.nil?
+      flash.now[:errors] = ['Invalid username or password.']
+      render :new
+    else
+    # Log them in and redirect them if we find them
+      login!(@user)
+      redirect_to user_url(@user)
+    end
+
+  end
 end
--- week_12/day_2/benchBnB/frontend/benchbnb.jsx
+++ week_12/day_2/benchBnB/frontend/benchbnb.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as sessionApiUtil from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  window.sessionApiUtil = sessionApiUtil
+  
 });
